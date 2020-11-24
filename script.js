ff11e85de908a5489f96b0372628247070dcf696
desta mulu <destish21@yahoo.com>
2020-11-23 18:52:46 -0600

    new file

---
+++ script.js
@@ -0,0 +1,11 @@
+setTimeout(function() {
+    console.log('Time is up!')
+}, 5000)
+
+// var div = document.getElementById('target')
+// var count = 100
+// setInterval(function(){
+//     count++
+//     div.innetText = count + 'seconds have passed'
+//     // console.log( count + " x seconds have passed")
+// }, 1000)
