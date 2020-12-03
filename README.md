9f2aa00019ef6df31535efffe3466c9eb484771f
desta mulu <destish21@yahoo.com>
2020-12-02 20:11:31 -0600

    updated files

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# API CODE-QUIZ
+# CODING-QUIZ
 This is Coding quiz home work I did an appication for the taking Coding quiz in cluding 5 questions with its choice and correct answer in Array terms.
  In my home work I included index.html, style.css, script.js and screen shot image
 
  For my style.css I used riferance from youtube and I made risponse page by media quiry.
  It also includes start quiz button, Next quiz button, result and timer button.
+I gave 17sec per question totaltime 85 second and startes count from 85second.
+![api04-codequiz](screenshot03.png)
 
-![API CODE-QUIZ](screenshot03.png)
-
-URL:  https://destish21.github.io/api04-codequiz/.
+URL:  https://destish21.github.io/api04-codequiz/
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var container = document.getElementById('quizContainer');
-console.log(container)
 var questionEl = document.getElementById('question');
 var time = document.getElementById('time-disply')
 var choiceA = document.getElementById('choiceA');
@@ -7,17 +6,14 @@ var choiceB = document.getElementById('choiceB');
 var choiceC = document.getElementById('choiceC');
 var choiceD = document.getElementById('choiceD');
 var nextButton = document.getElementById('nextButton');
-var timeDisplayArea =document.getElementById("timeDispayArea")
+var timeDisplayArea = document.getElementById("timeDispayArea")
 var startButton = document.getElementById('start-btn')
 var result = document.getElementById('result');
 
-var currentQuestion = 0
-var score = 0
-var totQuestion = question.length;
 var count = 17;
 var timePerQuestion = 17;
 
-var questions =  
+var questions =
   [
     {
       question: "What does HTMl stand for?",
@@ -74,29 +70,26 @@ var questions =
 
   ];
 
-  var timeTotal = (17 * questions.length);
+var currentQuestion = 0
+var score = 0
+var totQuestion = questions.length;
 
+var timeTotal = (17 * questions.length);
 
 startButton.addEventListener('click', StartGame);
-function  StartGame() {
-  // console.log("ITS ALIVE")
+function StartGame() {
   questionEl.textContent = "currentquestion";
   currentQuestion = 0;
   loadQuestion(currentQuestion)
-
 }
 
 function loadQuestion(questionIndex) {
   var q = questions[questionIndex];
-  // console.log("DESTA", q)
   questionEl.textContent = (questionIndex + 1) + ',' + q.question;
   choiceA.textContent = q.answers.a
   choiceB.textContent = q.answers.b
-  // console.log(q.choiceC)
-  // console.log(choiceC)
   choiceC.textContent = q.answers.c
   choiceD.textContent = q.answers.d
-
   time = timeTotal;
   totalTimerInterval = setInterval(function () {
     time = time - 1;
@@ -110,7 +103,6 @@ function loadQuestion(questionIndex) {
 };
 
 function nextQuestionClick() {
-  //start btn click clears the front page container to be replaced by questions and start timer
   nextQuestion.addEventListener("click", function () {
     questionContainer.textContent = "";
     currentQuestion = 0;
@@ -126,7 +118,7 @@ function loadNextQuestion(totQuestions) {
   }
   var answer = selectedChoice.value;
   if (questions[currentQuestion].answers == answer) {
-    score += 10;
+    score += 5;
 
   }
   selectedChoice.checked = false;
@@ -142,4 +134,3 @@ function loadNextQuestion(totQuestions) {
   }
   loadQuestion(currentQuestion);
 }
-loadNextQuestion(question);
